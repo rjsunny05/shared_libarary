7a1e3428bd8918a29f8cb0985cdff9e3c8ec8597
rjsunnny <rajeevsunny05@gmail.com>
2025-01-27 06:16:29 +0530

    Create ansible-config.groovy

---
+++ resources/ansible-config.groovy
@@ -0,0 +1,7 @@
+return [
+    SLACK_CHANNEL_NAME  : 'all-jenslack',
+    ENVIRONMENT         : 'prod',
+    CODE_BASE_PATH      : '/home/ansible',
+    ACTION_MESSAGE      : 'Deployment completed for production environment!',
+    KEEP_APPROVAL_STAGE : true
+]
